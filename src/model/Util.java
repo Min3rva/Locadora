2aa442bda653416b61f26c5a49e6b0258ce8ad76
Min3rva <talita_matta@hotmail.com>
2016-02-19 16:18:43 -0300

    Valida placa de carro

--- src/model/Util.java
+++ src/model/Util.java
@@ -2,13 +2,33 @@ package model;
 
 public class Util {
 	
-	public static boolean validarCPF(String cpf){
+	public static boolean validarCPF(String cpf) {
 		//TODO
 		return false;
 	}
 	
 	public static boolean validarPlacaCarro(String placa){
-		//TODO
-		return false;
+		
+		if(placa.length() == 7){
+			
+			placa.toUpperCase();
+			
+			System.out.println(placa);
+			
+			for(int i=0; i<3; i++){
+				if( !((placa.charAt(i) >= 'A') && (placa.charAt(i) <= 'Z')) ){
+					return false;
+				}
+			}
+			
+			for(int i=3; i<7; i++){
+				if( !((placa.charAt(i) >= '0') || (placa.charAt(i) <= '9')) ){
+					return false;
+				}
+			}
+		}
+		else return false;
+		
+		return true;
 	}
 }
