9862f4e6acc0babbf8ba63320535065a6d4d085e
Min3rva <talita_matta@hotmail.com>
2016-02-18 23:15:31 -0300

    Divisão de implementação da classe Sistema

--- src/system/Sistema.java
+++ src/system/Sistema.java
@@ -4,34 +4,42 @@ import model.*;
 
 public class Sistema {
 	
+	//zézis
 	public static Cliente cadastrarCliente() {
 		return null;
 	}
 	
+	//zézis
 	public static Carro cadastrarCarro(){
 		return null;
 	}
 	
+	//talitis
 	public static Aluguel alugarCarro(){
 		return null;
 	}
 	
+	//talitis
 	public static void devolverCarro(){
 		
 	}
 	
+	//zézis
 	public static String listarClientes(){
 		return null;
 	}
 	
+	//zézis
 	public static String listarCarros(){
 		return null;
 	}
 	
+	//talitis
 	public static String listarAlugueisFinalizados(){
 		return null;
 	}
-
+	
+	//talitis
 	public static String listarAlugueisHoje(){
 		return null;
 	}
