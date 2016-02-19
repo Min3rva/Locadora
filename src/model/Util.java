50d8467264f7ab58083bc793a3cb63e3dacb3d14
Min3rva <talita_matta@hotmail.com>
2016-02-19 01:29:36 -0300

    Adicionando classe util

---
+++ src/model/Util.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Util {
+	
+	public static boolean validarCPF(String cpf){
+		//TODO
+		return false;
+	}
+	
+	public static boolean validarPlacaCarro(String placa){
+		//TODO
+		return false;
+	}
+}
